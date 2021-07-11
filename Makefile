c27170604d03606e5717b3dc042a0c5245f02892
Max Block <maxblock@pm.me>
2021-07-11 21:03:35 +0300

    update

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
 docker-build:
 	docker build --tag zero2prod --file Dockerfile .
+
+
+docker-run:
+	docker run -p 3000:3000 zero2prod
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use sqlx::PgPool;
+use sqlx::{postgres::PgPoolOptions};
 use std::net::TcpListener;
 use zero2prod::{
     configuration::get_configuration,
@@ -11,10 +11,13 @@ async fn main() -> std::io::Result<()> {
     let subscriber = get_subsriber("zero2prod".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
-    let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool =
-        PgPool::connect(&configuration.database.connection_string()).await.expect("Failed to connect to Postgres.");
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let conf = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect(&conf.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
+    let address = format!("{}:{}", conf.application.host, conf.application.port);
     let listener = TcpListener::bind(address).expect("Failed to bind 3000 port");
     run(listener, connection_pool)?.await
 }
